730e01e9fe987a5b8faddb67caaa395fc64f7247
Luzzi Charly <luzzi.charly@gmail.com>
2019-06-18 21:47:32 +0200

    Create nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,17 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Smoke-Paradise') }}</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @yield('css')
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name', 'Smoke-Paradize') }}</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="navbar-header w-100 d-flex justify-content-between">
+        <a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name', 'Smoke-Paradize') }}</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToBurger" aria-controls="navbarToBurger" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+    <div class="collapse navbar-collapse" id="navbarToBurger">
         <ul class="navbar-nav ml-auto">
             @guest
                 <li class="nav-item{{ currentRoute(route('login')) }}"><a class="nav-link" href="{{ route('login') }}">@lang('Connexion')</a></li>
@@ -28,6 +30,42 @@
                     </form>
                 </li>
             @endguest
+        </ul>
+    </div>
+    <div class="d-flex align-items-center">
+        <div class="pouch-container">
+            <img src="{{ asset('images/pouch.png') }}" alt="" class="img-fluid w-75">
+            <div class=""><p class="m-0">2</p></div>
+        </div>
+        <div class="user-container">
+            <i class="far fa-user fa-2x"></i>
+        </div>
+    </div>
+</nav>
+
+<nav class="navbar sticky-top navbar-expand-lg navbar-light bg-green">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Briquets</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Feuilles à rouler</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Filtres
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
         </ul>
     </div>
 </nav>
