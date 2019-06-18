f91fb167f2d23da345ea710a52a27a3c6a70d5e4
Luzzi Charly <luzzi.charly@gmail.com>
2019-06-18 02:40:17 +0200

    Create forms / Edit routes, controllers

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,8 @@
     @yield('css')
 </head>
 <body>
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark">
-    <a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name', 'Album') }}</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name', 'Smoke-Paradize') }}</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,6 @@
 |
 */
 
-Auth::routes();
+Auth::routes(['verify' => true]);
+
 Route::get('/', 'HomeController@index')->name('home');
